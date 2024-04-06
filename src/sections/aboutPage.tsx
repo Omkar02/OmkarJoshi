5877a69770bbc27d794a484104cecfc118bee243
Omkar02 <omkjoshi.dev.07@gmail.com>
2024-04-06 20:53:28 +0530

    working animation for window through

--- src/sections/aboutPage.tsx
+++ src/sections/aboutPage.tsx
@@ -7,13 +7,15 @@ interface AboutPageInterface {
 export default function AboutPage({ imageXCalc }: AboutPageInterface) {
     return (
         <section>
-            <div className="mt-[-200vh] h-[200vh] overflow-clip bg-blue-300 pb-20">
+            <div className="mt-[-200vh] h-[200vh] overflow-clip bg-zinc-900 pb-20">
                 <motion.span
                     style={{ x: imageXCalc }}
-                    className="sticky top-1/2 mx-auto block aspect-video w-[1600px] max-w-[90%] rounded-[60px] bg-gray-300 shadow-2xl md:top-1/4"
+                    className="sticky top-[50%] mx-auto block aspect-video 
+                    w-[1200px] max-w-[90%] rounded-[60px] 
+                    border-4 border-white bg-lime-300 md:top-1/4"
                 />
             </div>
-            <div className="space-y-[80px] bg-blue-100 md:text-[300px]">
+            <div className="space-y-[80px] bg-lime-300 md:text-[300px]">
                 <p>Some more content</p>
                 <p>So there's</p>
                 <p>Some room</p>
--- src/sections/heroSection.tsx
+++ src/sections/heroSection.tsx
@@ -11,54 +11,47 @@ import {
     useTransform,
 } from "framer-motion";
 
-interface windowInterface {
-    windowFor: "mobile" | "desktop";
-    classname: string;
-    ref: React.MutableRefObject<null>;
-}
-interface heroInterface {
-    ref: React.MutableRefObject<null>;
-}
+// interface windowInterface {
+//     ref: React.MutableRefObject<null>;
+// }
+// interface heroInterface {
+//     ref: React.MutableRefObject<null>;
+// }
 
-function Window({
-    windowFor = "mobile",
-    classname = "",
-    ref,
-}: windowInterface) {
-    const visiblity = windowFor == "mobile" ? "lg:hidden" : "hidden lg:block";
+function Window() {
     return (
-        <div className={`${classname} ${visiblity}`} ref={ref}>
+        <div className="">
             <div
-                className="mx-auto aspect-[5/8] w-[16rem] 
-                md:w-[24rem] lg:mr-3 lg:w-[26rem]"
+                className="mx-auto aspect-[8/5] w-[15rem] 
+                md:w-[24rem] lg:mr-3 lg:aspect-[5/8] lg:w-[26rem]"
             ></div>
         </div>
     );
 }
 
-function Hero({ ref }: heroInterface) {
+function Hero() {
     return (
         <>
             <section
                 className="mx-auto flex h-full 
-                flex-col-reverse items-center justify-around 
-                text-center lg:flex-row lg:text-left"
+                flex-col items-center justify-center text-center 
+                lg:flex-row lg:justify-around lg:text-left"
             >
-                <div className="mx-5 grid gap-2 lg:max-w-4xl">
-                    <p className="text-md text-zinc-400 sm:text-xl lg:text-2xl">
+                <div className="mx-5 grid sm:gap-2 lg:max-w-4xl">
+                    <p className="text-sm text-zinc-400 sm:text-xl lg:text-2xl">
                         Full Stack Developer
                     </p>
-                    <h1 className="text-3xl font-bold md:text-5xl lg:text-6xl xl:text-8xl">
+                    <h1 className="text-xl font-bold sm:text-3xl md:text-5xl lg:text-6xl xl:text-8xl">
                         Crafting Exceptional Web Experiences With React, Python
                         & Node.Js
                     </h1>
-                    <p className="mt-1 font-mono text-base md:mt-5 md:text-xl lg:text-2xl">
+                    <p className="mt-1 font-mono text-sm sm:text-base md:mt-5 md:text-xl lg:text-2xl">
                         I Design, Develop, & Deploy Web Applications.
                         Specializing In User-Friendly Interfaces & Robust
                         Back-End Solutions.
                     </p>
                 </div>
-                <Window windowFor="desktop" classname="" ref={ref} />
+                <Window />
             </section>
         </>
     );
@@ -77,29 +70,25 @@ export default function HeroSection() {
 
     const scale = useTransform(scrollYprocessSpring, [0, 1], [1, 12]);
     const imageX = useTransform(scrollYprocessSpring, [0, 1], [50, 0]);
-    const imageXCalc = useMotionTemplate`max(0, calc(${imageX}% + calc(${imageX}vw - 300px)))`;
+    const imageXCalc = useMotionTemplate`max(0px, calc(${imageX}% + calc(${imageX}vw - 300px)))`;
 
     return (
         <>
-            <div className="relative z-10 h-[200vh] overflow-clip">
+            <div ref={ref} className="relative z-10 h-[200vh]  overflow-clip">
                 <motion.div
                     style={{ scale }}
-                    className="hero-background sticky left-0 top-0 h-screen 
-                    origin-[50%_50%] md:origin-[90%_40%]"
+                    className="left-0 top-0 h-dvh 
+                    origin-[50%_50%] md:origin-[90%_40%] "
                 >
+                    <div className="window-mask absolute -z-20 h-dvh w-full bg-zinc-900"></div>
                     <div className="window-mask">
-                        <section className="grid-rows-13 mx-2 grid h-screen md:mx-5 lg:mx-8">
+                        <section className="grid-rows-13 mx-2 grid h-dvh md:mx-5 lg:mx-8">
                             <Header className="row-span-1" />
-                            <main className="row-span-7 rounded-3xl bg-zinc-100 shadow-inner lg:row-span-9">
-                                <Hero ref={ref} />
+                            <main className="row-span-8 rounded-3xl bg-zinc-100 shadow-inner lg:row-span-9">
+                                <Hero />
                             </main>
-                            <Footer className="row-span-6 mb-2 lg:row-span-3" />
+                            <Footer className="row-span-4 lg:row-span-3" />
                         </section>
-                        {/* <Window
-                            windowFor="mobile"
-                            classname="h-screen"
-                            ref={ref}
-                        /> */}
                     </div>
                 </motion.div>
             </div>
